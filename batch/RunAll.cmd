234348e186a731cf2843263ae9cd262482e04d0a
BartRijken <bartrijken@gmail.com>
2025-12-29 12:00:20 +0100

    WIP waterberging

--- batch/RunAll.cmd
+++ batch/RunAll.cmd
@@ -36,7 +36,7 @@ goto runScenarios
 :runPrepareBasedata
 
 REM deletes the old BaseData folder
-rmdir %LocalDataProjDir%\Basedata /s /q 
+REM rmdir %LocalDataProjDir%\Basedata /s /q 
 
 call ..\batch\RunImpl.cmd %ProjDir%\cfg\main.dms /WriteBasedata/Generate_Run1
 echo "ErrorLevel is " %ErrorLevel% 
@@ -45,7 +45,7 @@ if %ErrorLevel% NEQ 0 goto ErrorEnd
 :runPrepareVariantdata
 
 REM deletes the old VariantData folder.
-rmdir %LocalDataProjDir%\VariantData /s /q 
+REM rmdir %LocalDataProjDir%\VariantData /s /q 
 
 set RSL_VARIANT_NAME=BAU
 call ..\batch\RunVariantData.cmd
--- batch/RunZichtjaren.cmd
+++ batch/RunZichtjaren.cmd
@@ -6,12 +6,12 @@ call ..\batch\RunImpl.cmd %ProjDir%\cfg\main.dms Allocatie/Zichtjaren/Y2040/Impl
 if %ErrorLevel% NEQ 0 goto ErrorEnd
 
 
-call ..\batch\RunImpl.cmd %ProjDir%\cfg\main.dms /Indicatoren/Basisjaar/Landgebruikskaart/Result_SA
-if %ErrorLevel% NEQ 0 goto ErrorEnd
-call ..\batch\RunImpl.cmd %ProjDir%\cfg\main.dms /Indicatoren/Y2030/Landgebruikskaart/Result_SA
-if %ErrorLevel% NEQ 0 goto ErrorEnd
-call ..\batch\RunImpl.cmd %ProjDir%\cfg\main.dms /Indicatoren/Y2040/Landgebruikskaart/Result_SA
-if %ErrorLevel% NEQ 0 goto ErrorEnd
+REM call ..\batch\RunImpl.cmd %ProjDir%\cfg\main.dms /Indicatoren/Basisjaar/Landgebruikskaart/Result_SA
+REM if %ErrorLevel% NEQ 0 goto ErrorEnd
+REM call ..\batch\RunImpl.cmd %ProjDir%\cfg\main.dms /Indicatoren/Y2030/Landgebruikskaart/Result_SA
+REM if %ErrorLevel% NEQ 0 goto ErrorEnd
+REM call ..\batch\RunImpl.cmd %ProjDir%\cfg\main.dms /Indicatoren/Y2040/Landgebruikskaart/Result_SA
+REM if %ErrorLevel% NEQ 0 goto ErrorEnd
 
 
 :runLastYear
